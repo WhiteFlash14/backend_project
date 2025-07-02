4fbf00445725e7003a50f2436ca53415f290f3c2
nish <nishchal1405@gmail.com>
2025-07-02 14:36:22 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({ 
     path: "../.env" 
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error :", error)
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("Failed to connect to the database:", error);
+     // Exit the process with failure
+});
 
 
 
