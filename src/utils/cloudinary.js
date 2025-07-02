dae1f47b16ab60e0b8585d1fc026c96297ec4a88
nish <nishchal1405@gmail.com>
2025-07-03 02:42:45 +0530

    added file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnClodinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) {
+            throw new Error("Local file path is required for upload.");
+        }
+
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto", // Automatically detect the resource type (image, video, etc.)
+        } );
+        
+        console.log("File uploaded successfully:", result.url);
+        return result;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // Clean up the local file if upload fails
+        return null;
+    }
+}
+
+export {uploadOnClodinary}
