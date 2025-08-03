b75f4afd159f00efffc14626c18e83292db21b0a
nish <nishchal1405@gmail.com>
2025-08-03 14:58:50 +0530

    fixed register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser  = asyncHandler(async (req, res) => {
    
 
    const {username, email, fullname, password} = req.body 
-   console.log("email", email);
+   //console.log("email", email);
 
    // if (fullname === ""){
    //    throw new ApiError("Fullname is required", 400);
@@ -37,7 +37,12 @@ const registerUser  = asyncHandler(async (req, res) => {
    }
 
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   let coverImageLocalPath ;
+   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path;
+   }
 
    if (!avatarLocalPath) {
       throw new ApiError("Avatar is required", 400);
